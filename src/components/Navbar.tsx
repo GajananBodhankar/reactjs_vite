157962e0d82ae9a7e3bbcee59fc07d394b8fb9a1
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-02-04 15:43:40 +0530

    Designed Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./Navstyle.css";
+function Navbar() {
+  const [show, setShow] = useState<boolean>(false);
+
+  return (
+    <>
+      <nav className="container">
+        <h1>Gajanan</h1>
+        <ul className="list">
+          <li className="underline">Home</li>
+          <li className="underline">Contact</li>
+          <li className="underline">About</li>
+        </ul>
+        <button className="menu" onClick={() => setShow(!show)}>
+          <span className="material-symbols-outlined">menu</span>
+        </button>
+      </nav>
+      {show && (
+        <div className="subContainer">
+          <button className="cross" onClick={() => setShow(!show)}>
+            <span className="material-symbols-outlined">close</span>
+          </button>
+          <ul className="buttonUl">
+            <li>Home</li>
+            <li>Contact</li>
+            <li>About</li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Navbar;
