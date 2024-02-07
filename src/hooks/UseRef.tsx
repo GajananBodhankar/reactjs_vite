2699da7101dfffb463c35745116d25aed137e042
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-02-07 16:42:22 +0530

    made a UI change

---
+++ src/hooks/UseRef.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useRef, useState } from "react";
+
+// function UseRef() {
+//   let [val, setVal] = useState(1000);
+//   let a = 0;
+//   useEffect(() => {
+//     console.log("rerendered and a", a);
+//   });
+//   return (
+//     <button
+//       onClick={() => {
+//         a = a + 1;
+//         console.log(a);
+//         setVal(val + 100);
+//       }}
+//     >
+//       click
+//     </button>
+//   );
+// }
+
+function UseRef() {
+  let [val, setVal] = useState(1000);
+  let a = useRef(0);
+  useEffect(() => {
+    console.log("rerendered and a", a);
+  });
+  return (
+    <button
+      onClick={() => {
+        a.current = a.current + 1;
+        console.log(a);
+        setVal(val + 100);
+      }}
+    >
+      click
+    </button>
+  );
+}
+
+export default UseRef;
