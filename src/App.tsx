36dd2b58a9ae9e8950e1d27b96e85c3a64bb5fc0
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-02-07 16:42:07 +0530

    Added useref

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar.tsx";
+import UseRef from "./hooks/UseRef.tsx";
 
 function App() {
   const [count, setCount] = useState(0);
-  return <Navbar />;
+  // return <Navbar />;
+  return <UseRef />;
 }
 
 export default App;
