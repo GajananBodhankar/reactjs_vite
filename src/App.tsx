c17ada58f8df98de7acc2fdeef62f08ece34a1d1
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-02-25 14:16:29 +0530

    Made some change

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import { useState } from "react";
-import Navbar from "./components/Navbar.tsx";
-import UseRef from "./hooks/UseRef.tsx";
-import UseMemo from "./hooks/useMemo.tsx";
-import UseCallback from "./hooks/UseCallback.tsx";
-import UseForm from "./hooks/UseForm.tsx";
-import { Provider } from "react-redux";
-import SagaStore from "./ReduxSaga/SagaStore.ts";
-import Index from "./ReduxSaga/Index.tsx";
+// import { useState } from "react";
+// import Navbar from "./components/Navbar.tsx";
+// import UseRef from "./hooks/UseRef.tsx";
+// import UseMemo from "./hooks/useMemo.tsx";
+// import UseCallback from "./hooks/UseCallback.tsx";
+// import UseForm from "./hooks/UseForm.tsx";
+// import { Provider } from "react-redux";
+// import SagaStore from "./ReduxSaga/SagaStore.ts";
+// import Index from "./ReduxSaga/Index.tsx";
 
-function App() {
-  const [count, setCount] = useState(0);
-  // return <Navbar />;
-  // return <UseRef />;
-  // return <UseMemo />;
-  // return <UseCallback />;
-  // return <UseForm />;
+// function App() {
+//   const [count, setCount] = useState(0);
+//   // return <Navbar />;
+//   // return <UseRef />;
+//   // return <UseMemo />;
+//   // return <UseCallback />;
+//   // return <UseForm />;
+//   return (
+//     <Provider store={SagaStore}>
+//       {/* <Index /> */}
+//       <Index />
+//     </Provider>
+//   );
+// }
+
+// export default App;
+
+import React, { useRef, useState } from "react";
+import { Virtual, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./App.css";
+
+export default function App() {
+  // Create array with 500 slides
+  const [slides, setSlides] = useState(
+    Array.from({ length: 500 }).map((_, index) => `Slide ${index + 1}`)
+  );
+
   return (
-    <Provider store={SagaStore}>
-      {/* <Index /> */}
-      <Index />
-    </Provider>
+    <>
+      <Swiper
+        modules={[Virtual, Navigation, Pagination]}
+        slidesPerView={7}
+        spaceBetween={30}
+        navigation={true}
+        virtual
+      >
+        {slides.map((slideContent, index) => (
+          <SwiperSlide key={slideContent} virtualIndex={index}>
+            {slideContent}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 }
-
-export default App;
