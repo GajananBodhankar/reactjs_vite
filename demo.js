5330d183cfb4f75cc1098bfe80cb18ca8c9128e6
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-02-25 12:58:01 +0530

    Made a change

--- demo.js
+++ demo.js
@@ -19,38 +19,8 @@
 // console.log(response(10));
 // console.log(response(10));
 
-[
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+check();
 
-// ===>
-
-[
-  [7, 4, 1],
-  [8, 5, 2],
-  [9, 6, 3],
-];
-
-async function transpose(mat) {
-  let arr = [];
-  for (let i = 0; i < mat.length; i++) {
-    let temp = [];
-    for (let j = 0; j < mat.length; j++) {
-      temp.push(mat[j][i]);
-    }
-    arr.push(temp.reverse());
-  }
-  console.log(arr);
-  let response = await fetch("https://dummyjson.com/products");
-  let data = await response.json();
-  // return data;
-  console.log(data);
+function check() {
+  console.log("hello");
 }
-
-transpose([
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-]);
