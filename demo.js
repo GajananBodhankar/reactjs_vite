5df9c8a22cea39bcfc11d8d6ab09fe69dfcec36a
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-06-07 17:07:26 +0530

    Made a change in class js

--- demo.js
+++ demo.js
@@ -1,26 +1,39 @@
-function sqaure(num) {
-  console.log("Square executed");
-  return num * num;
+// function sqaure(num) {
+//   console.log("Square executed");
+//   return num * num;
+// }
+
+// function memo(fn) {
+//   let obj = {};
+//   return function (input) {
+//     if (obj[input]) {
+//       return obj[input];
+//     } else {
+//       obj[input] = fn(input);
+//       return obj[input];
+//     }
+//   };
+// }
+
+// let response = memo(sqaure);
+// console.log(response(10));
+// console.log(response(10));
+
+// check();
+// // Hoisting
+// function check() {
+//   console.log("hello");
+// }
+
+class A {
+  constructor(a, b) {
+    this.name = a;
+    this.age = b
+  }
+  disp() {
+    console.log(this.name, this.age)
+  }
 }
 
-function memo(fn) {
-  let obj = {};
-  return function (input) {
-    if (obj[input]) {
-      return obj[input];
-    } else {
-      obj[input] = fn(input);
-      return obj[input];
-    }
-  };
-}
-
-let response = memo(sqaure);
-console.log(response(10));
-console.log(response(10));
-
-check();
-// Hoisting
-function check() {
-  console.log("hello");
-}
+let i = new A("gsjsnsn", 22);
+i.disp()
