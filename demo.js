c61bcd786bf73e90a6f5d05bd30698e2af90c0ba
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-11-23 21:58:48 +0530

    made a change

--- demo.js
+++ demo.js
@@ -1,21 +1,31 @@
-const join = (a, b, c) => {
-  return `${a}_${b}_${c}`;
-};
+// const join = (a, b, c) => {
+//   return `${a}_${b}_${c}`;
+// };
 
-let _ = curry.placeholder;
-function curry(fn) {
-  return function curried(...args) {
-    if (fn.length <= args.length && !args.includes(_)) {
-      return fn(...args);
-    } else {
-      return function (...arg2) {
-        console.log(args, arg2);
-        return curried.apply(this, args.concat(arg2));
-      };
-    }
-  };
+// let _ = curry.placeholder;
+// function curry(fn) {
+//   return function curried(...args) {
+//     if (fn.length <= args.length && !args.includes(_)) {
+//       return fn(...args);
+//     } else {
+//       return function (...arg2) {
+//         console.log(args, arg2);
+//         return curried.apply(this, args.concat(arg2));
+//       };
+//     }
+//   };
+// }
+// curry.placeholder = Symbol();
+
+// let result = curry(join);
+// console.log(result(_, _, _)(1)(_, 3)(2));
+
+function check() {
+  throw new Error("Pass an argument");
 }
-curry.placeholder = Symbol();
 
-let result = curry(join);
-console.log(result(_, _, _)(1)(_, 3)(2));
+function getData(arg = check()) {
+  console.log(arg);
+}
+
+getData(null);
--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,17 @@
-// import React, { useEffect, useRef, useState } from "react";
-// function reducer(state = { count: 0 }, action) {
-//   switch (action.type) {
-//     case "Increment": {
-//       return { count: (state.count ?? 0) + 1 };
-//     }
-//     case "Decrement": {
-//       return { count: (state.count ?? 0) - 1 };
-//     }
-//   }
-// }
-
-// function createStore(reducer) {
-//   let state = { count: 0 },
-//     subscribers = [];
-//   function getState() {
-//     return state;
-//   }
-//   function dispatch(action) {
-//     state = reducer(state, action);
-//     console.log(state);
-//     subscribers.forEach((i) => {
-//       console.log(i);
-//       i(state.count);
-//     });
-//     console.log(state);
-//   }
-//   function subscribe(callback) {
-//     subscribers.push(callback);
-//   }
-//   return { getState, dispatch, subscribe };
-// }
-
-// const store = createStore(reducer);
-
-// function App() {
-//   const state = store.getState();
-//   const [count, setCount] = useState(state.count);
-//   useEffect(() => {
-//     store.subscribe((value) => setCount(value));
-//   }, []);
-
-//   return (
-//     // <div>
-//     //   <button
-//     //     onClick={() => {
-//     //       store.dispatch({ type: "Increment" });
-//     //       // setCount(state.count);
-//     //     }}
-//     //   >
-//     //     increment
-//     //   </button>
-//     //   <p>{count}</p>
-//     // </div>
-//     <TrafficLi
-//   );
-// }
-
-// export default App;
-
-// import React, { useEffect, useRef, useState } from "react";
-// import TrafficLights from "./TrafficLights/TrafficLights";
-// import Debouncing from "./hooks/useDebounce";
-import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import { GraphQlWrapper } from "./Graphql/Graphql";
-import FirstRender from "./hooks/UseIsFirstRender";
-import MemoryGame from "./MemoryGame/MemoryGame";
-import ProgressBar from "./ProgressBar/ProgressBar";
-function App() {
-  const [count, setCount] = useState(0);
-  const result = useRef<any>(null);
-  useEffect(() => {
-    result.current = setInterval(() => setCount((prev) => prev + 1), 100);
-    console.log(result);
-  }, []);
+import useToggle from "./useToggle";
 
-  
-  if (count == 100) {
-    clearInterval(result.current);
-  }
-  // console.log(count, result);
-  // return <TrafficLights />;
-  // return <Debouncing />;
-  // return <ProgressBar />;
-  // return <MemoryGame />;
-  // return <FirstRender />;
-  // return <GraphQlWrapper />;
-  return <div>{count} </div>;
+function App() {
+  const [isDark, toggleDark] = useToggle();
+  const [isVisible, toggleVisible] = useToggle();
+  return (
+    <div>
+      <p>Mode {isDark ? "true" : "false"}</p>
+      <button onClick={toggleDark}>Toggle mode</button>
+      <p>Visibility is {isVisible ? "true" : "false"}</p>
+      <button onClick={toggleVisible}>Toggle Visibility</button>
+    </div>
+  );
 }
 
 export default App;
