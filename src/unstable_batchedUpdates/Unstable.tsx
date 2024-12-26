999ed7f3336b3e308c0f5e54cc0a8522a8709cde
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-12-26 12:15:10 +0530

    made a change

--- src/unstable_batchedUpdates/Unstable.tsx
+++ src/unstable_batchedUpdates/Unstable.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Unstable() {
-  return <div>Unstable</div>;
+  const [count, setCount] = useState(0);
+  const [bool, setBool] = useState(false);
+  function handleCount() {
+    setTimeout(() => {
+      setCount(count + 1);
+      setBool(!bool);
+    });
+  }
+  console.log("re-rendered");
+  return (
+    <div>
+      Unstable,
+      {count}
+      {bool ? "true" : "false"}
+      <button onClick={handleCount}>Click</button>
+    </div>
+  );
 }
 
 export default Unstable;
