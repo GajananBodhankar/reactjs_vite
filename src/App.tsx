831550962bf02456eb2b14a30228f8e1125d0cf2
GajananBodhankar <bodhankargajanan99@gmail.com>
2025-03-09 13:55:52 +0530

    worker threads

--- src/App.tsx
+++ src/App.tsx
@@ -1,158 +1,189 @@
-// import React, {
-//   useState,
-//   useCallback,
-//   useEffect,
-//   useRef,
-//   useMemo,
-// } from "react";
-// import useCustomHook from "./useCustomHook";
+// // import React, {
+// //   useState,
+// //   useCallback,
+// //   useEffect,
+// //   useRef,
+// //   useMemo,
+// // } from "react";
+// // import useCustomHook from "./useCustomHook";
+
+// // function App() {
+// //   const [count, setCount] = useState(0);
+// //   const [bool, setBool] = useState(false);
+// //   const ref = useRef<any>();
+// //   // const square = useCallback(() => {
+// //   //   // console.log("first");
+// //   //   setBool((prev) => !prev);
+// //   //   // console.log("in callback");
+// //   // }, [count]);
+// //   // useEffect(() => {
+// //   //   console.log(ref.current == square);
+// //   //   ref.current = square;
+// //   // });
+// //   const square = useCustomHook(() => {
+// //     console.log("first", count * count);
+// //   }, [count]);
+// //   // useEffect(() => {
+// //   //   console.log(ref.current == square);
+// //   //   ref.current = square;
+// //   // });
+
+// //   return (
+// //     <div>
+// //       <h2
+// //         ref={ref}
+// //         onMouseEnter={() => console.log("first")}
+// //         onMouseLeave={() => console.log("last")}
+// //       >
+// //         Count is {count}
+// //       </h2>
+// //       {/* <h5>The square is {square}</h5> */}
+// //       <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+// //       <button onClick={() => setBool((prev) => !prev)}>Change State</button>
+// //       <p>{bool ? "true" : "false"}</p>
+// //       <button onClick={() => square()}>x</button>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+// import React, { Component, useCallback, useEffect, useState } from "react";
+// import { Ref, useRef } from "react";
+// import useCustomEffect from "./hooks/UseEffect";
+// import useArray from "./hooks/useArray";
+// import Parent from "./ForwardRef/Parent";
+// import ImperativeParent from "./useImperativeHandle/ImperativeParent";
+// import Unstable from "./unstable_batchedUpdates/Unstable";
+// import Page from "./Classnames/Page";
+// import { useTimeout } from "./useTimeout/useTimeout";
+// import usePrevious from "./hooks/usePrevious";
+// import GridLights from "./GridLights/GridLights";
+// import LifecycleThis from "./ClassThis/LifecycleThis";
+// import { useNavigate, useNavigation } from "react-router-dom";
+// import {refres} from 'react-router'
+
+// export function useHover<T extends HTMLElement>(): [Ref<T>, boolean] {
+//   const [hovered, setHovered] = useState(false);
+//   const ref = useRef<any>();
+//   useEffect(() => {
+//     setHovered(false);
+//     ref.current.addEventListener("mouseenter", () => {
+//       setHovered(true);
+//     });
+//     ref.current.addEventListener("mouseleave", () => {
+//       setHovered(false);
+//     });
+//     return () => {
+//       ref.current.removeEventListener("mouseenter", () => {
+//         setHovered(true);
+//       });
+//       ref.current.removeEventListener("mouseleave", () => {
+//         setHovered(false);
+//       });
+//     };
+//   }, [ref.current]);
+//   return [ref, hovered];
+// }
+
+// // if you want to try your code on the right panel
+// // remember to export App() component like below
+
+// interface Iobj {
+//   value: any;
+//   deps: [];
+// }
+
+// function useCallbackCustom(this: any, cb: () => void, args: Readonly<{}>[]) {
+//   let refer = React.createRef.bind<any>(this);
+//   if (
+//     !refer.current ||
+//     JSON.stringify(args) !== JSON.stringify(refer.current?.args)
+//   ) {
+//     console.log(
+//       JSON.stringify(refer?.current?.args),
+//       args,
+//       "prev",
+//       refer.current
+//     );
+//     refer.current = { value: cb, args };
+//     console.log(
+//       JSON.stringify(refer?.current?.args),
+//       args,
+//       "args",
+//       refer.current
+//     );
+//   }
+//   return refer?.current?.value;
+// }
 
 // function App() {
-//   const [count, setCount] = useState(0);
-//   const [bool, setBool] = useState(false);
-//   const ref = useRef<any>();
-//   // const square = useCallback(() => {
-//   //   // console.log("first");
-//   //   setBool((prev) => !prev);
-//   //   // console.log("in callback");
-//   // }, [count]);
-//   // useEffect(() => {
-//   //   console.log(ref.current == square);
-//   //   ref.current = square;
-//   // });
-//   const square = useCustomHook(() => {
-//     console.log("first", count * count);
-//   }, [count]);
-//   // useEffect(() => {
-//   //   console.log(ref.current == square);
-//   //   ref.current = square;
-//   // });
+//   // const timer = useTimeout(()=>console.log("first"),3000);
+//   // console.log(timer)
+//   // const [count,set]=useState(0)
+//   // return <div>App
+//   //   <button onClick={()=>set(10)}>click</button>
+//   // </div>;
+//   // const [c, s] = useState(0);
+//   // const prev = usePrevious(c);
+
+//   useEffect(() => {
+//     const controller = new AbortController();
+//     document.addEventListener(
+//       "click",
+//       (e) => {
+//         console.log("clicked");
+//       },
+//       { signal: controller.signal }
+//     );
+//     document.addEventListener("click", (e) => console.log("first"), {
+//       signal: controller.signal,
+//     });
+//     // return () => {
+//     // document.removeEventListener("click", (e) => {
+//     //   console.log("removed");
+//     // });
+
+//     // };
+//     controller.abort();
+//   }, []);
 
 //   return (
-//     <div>
-//       <h2
-//         ref={ref}
-//         onMouseEnter={() => console.log("first")}
-//         onMouseLeave={() => console.log("last")}
-//       >
-//         Count is {count}
-//       </h2>
-//       {/* <h5>The square is {square}</h5> */}
-//       <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
-//       <button onClick={() => setBool((prev) => !prev)}>Change State</button>
-//       <p>{bool ? "true" : "false"}</p>
-//       <button onClick={() => square()}>x</button>
-//     </div>
+//     // <div>
+//     //   {prev}
+//     //   <button onClick={() => s(c + 1)}>Click</button>
+//     // </div>
+//     // <GridLights />
+//     // <LifecycleThis/>
+//     <div>Hello</div>
 //   );
 // }
 
 // export default App;
 
-import React, { Component, useCallback, useEffect, useState } from "react";
-import { Ref, useRef } from "react";
-import useCustomEffect from "./hooks/UseEffect";
-import useArray from "./hooks/useArray";
-import Parent from "./ForwardRef/Parent";
-import ImperativeParent from "./useImperativeHandle/ImperativeParent";
-import Unstable from "./unstable_batchedUpdates/Unstable";
-import Page from "./Classnames/Page";
-import { useTimeout } from "./useTimeout/useTimeout";
-import usePrevious from "./hooks/usePrevious";
-import GridLights from "./GridLights/GridLights";
-import LifecycleThis from "./ClassThis/LifecycleThis";
+import { useEffect, useState } from "react";
 
-export function useHover<T extends HTMLElement>(): [Ref<T>, boolean] {
-  const [hovered, setHovered] = useState(false);
-  const ref = useRef<any>();
+const Component = () => {
+  const [sum, setSum] = useState(0);
   useEffect(() => {
-    setHovered(false);
-    ref.current.addEventListener("mouseenter", () => {
-      setHovered(true);
+    const worker = new Worker(new URL("../demo.js", import.meta.url), { type: "module" });
+    worker.postMessage("message");
+    worker.addEventListener("message", (e) => {
+      setSum(e.data);
     });
-    ref.current.addEventListener("mouseleave", () => {
-      setHovered(false);
-    });
-    return () => {
-      ref.current.removeEventListener("mouseenter", () => {
-        setHovered(true);
-      });
-      ref.current.removeEventListener("mouseleave", () => {
-        setHovered(false);
-      });
-    };
-  }, [ref.current]);
-  return [ref, hovered];
-}
-
-// if you want to try your code on the right panel
-// remember to export App() component like below
-
-interface Iobj {
-  value: any;
-  deps: [];
-}
-
-function useCallbackCustom(this: any, cb: () => void, args: Readonly<{}>[]) {
-  let refer = React.createRef.bind<any>(this);
-  if (
-    !refer.current ||
-    JSON.stringify(args) !== JSON.stringify(refer.current?.args)
-  ) {
-    console.log(
-      JSON.stringify(refer?.current?.args),
-      args,
-      "prev",
-      refer.current
-    );
-    refer.current = { value: cb, args };
-    console.log(
-      JSON.stringify(refer?.current?.args),
-      args,
-      "args",
-      refer.current
-    );
-  }
-  return refer?.current?.value;
-}
+  }, []);
+  return <p>Hello Component {sum}</p>;
+};
 
 function App() {
-  // const timer = useTimeout(()=>console.log("first"),3000);
-  // console.log(timer)
-  // const [count,set]=useState(0)
-  // return <div>App
-  //   <button onClick={()=>set(10)}>click</button>
-  // </div>;
-  // const [c, s] = useState(0);
-  // const prev = usePrevious(c);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    document.addEventListener(
-      "click",
-      (e) => {
-        console.log("clicked");
-      },
-      { signal: controller.signal }
-    );
-    document.addEventListener("click", (e) => console.log("first"), {
-      signal: controller.signal,
-    });
-    // return () => {
-    // document.removeEventListener("click", (e) => {
-    //   console.log("removed");
-    // });
-
-    // };
-    controller.abort();
-  }, []);
+  const [sum, setSum] = useState(0);
   return (
-    // <div>
-    //   {prev}
-    //   <button onClick={() => s(c + 1)}>Click</button>
-    // </div>
-    // <GridLights />
-    // <LifecycleThis/>
-    <div>Hello</div>
+    <div>
+      App
+      {sum}
+      <button onClick={() => setSum((prev) => prev + 1)}>Click</button>
+      <Component />
+    </div>
   );
 }
 
