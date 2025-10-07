71f029e964463e9c893706c76d304cafdb7d0ddb
GajananBodhankar <bodhankargajanan99@gmail.com>
2025-10-07 20:36:11 +0530

    done with useDeferredValue

--- src/hooks/useDeferredValue.tsx
+++ src/hooks/useDeferredValue.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useDeferredValue, useState } from "react";
 
-function useDeferredValue() {
+function UseDeferredValue() {
+  const [name, setName] = useState("");
+  const value=useDeferredValue(name)
   return (
-    <div>useDeferredValue</div>
-  )
+    <div>
+      <input type="text" className="w-full bg-amber-200 border-amber-300" value={name} onChange={(e) => setName(e.target.value)} />
+      {[...Array.from(new Array(10))].map((i) => (
+        <Component name={value} />
+      ))}
+    </div>
+  );
 }
 
-export default useDeferredValue
+const Component = ({ name }: { name: string }) => {
+  let i = 0;
+  while (i < 100000000) {
+    i++;
+  }
+  return (
+    <div>
+      <p className="text-red-300">Name is {name}</p>
+    </div>
+  );
+};
+
+export default UseDeferredValue;
