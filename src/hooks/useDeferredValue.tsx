6cfa93b8dcf158c27581e0711ebc13fe19c5ee33
GajananBodhankar <bodhankargajanan99@gmail.com>
2025-10-08 19:55:11 +0530

    Made a change in useDeferred value

--- src/hooks/useDeferredValue.tsx
+++ src/hooks/useDeferredValue.tsx
@@ -1,11 +1,21 @@
 import React, { useDeferredValue, useState } from "react";
-
+// here, while user is giving an input to the input as "Gajananananana"
+// while he/she is typing, the <Component/> will not re-render, it will re-render
+// with the latest value Gajananananana and that to only once i.e when user stops typing
+// useDeferredValue - while the value in it is updating continuously, do not take the updating value
+// till the time, use a deferred value, and once the updating is finished, remove the deferred value and 
+// take the latest value. 
 function UseDeferredValue() {
   const [name, setName] = useState("");
-  const value=useDeferredValue(name)
+  const value = useDeferredValue(name);
   return (
     <div>
-      <input type="text" className="w-full bg-amber-200 border-amber-300" value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        type="text"
+        className="w-full bg-amber-200 border-amber-300"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       {[...Array.from(new Array(10))].map((i) => (
         <Component name={value} />
       ))}
